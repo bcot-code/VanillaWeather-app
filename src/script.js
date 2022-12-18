f9696810dfa20c1727ce435aca88f38be2b0dff3
Barbie <78989054+bcot-code@users.noreply.github.com>
2022-12-18 06:31:22 -0600

    update 1

--- src/script.js
+++ src/script.js
@@ -1,54 +1,81 @@
-//Change of city...
-let weather = {
-  apiKey: "b9ba0314a93083136d968577c718e31d",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=imperial&appid=" +
-        this.apiKey
-    )
-      .then((response) => response.json())
-      .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp } = data.main;
-    console.log(name, icon, description, temp);
-    document.querySelector(".city").innerHTML = name;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-    document.querySelector(".description").innerHTML = description;
-    document.querySelector("#temperature").innerHTML = temp;
-  },
-};
 // Location
-function showPosition(position) {
-  let apiKey = "b9ba0314a93083136d968577c718e31d";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}`;
-  axios.get(apiUrl).then(weather);
-}
-// Change of F to C
-function convertTf(event) {
-  event.preventDefault();
-  celLink.classList.remove("active");
-  fehLink.classList.add("active");
-  let temperaElment = document.querySelector("#temperature");
-  let tempF = (displayWeather * 9) / 5 + 32;
-  temperaElment.innerHTML = Math.round(tempF);
-}
-function convertTc(event) {
-  event.preventDefault();
-  fehLink.classList.remove("active");
-  celLink.classList.add("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(displayWeather);
+
+function showTemperature(response) {
+  const { name } = response.data;
+  const { icon, description } = response.data.weather[0];
+  const { temp } = response.data.main;
+  document.querySelector(".city").innerHTML = name;
+  document.querySelector(".icon").src =
+    "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+  document.querySelector(".description").innerHTML = description;
+  document.querySelector("#temperature").innerHTML = temp;
+  windElment.innerHTML = Math.round(response.data.wind.speed);
+  document.body.style.backgroundImage =
+    "url('https://source.unsplash.com/1600x900/?" + name + ")";
 }
 
-let fehLink = document.getElementById("#Feh");
-fehLink.addEventListener("click", converTf);
-let celLink = document.getElementById("#Cel");
-celLink.addEventListener("click", convertTc);
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector(".search-bar");
+  search(cityInputElement.value);
+  let form = document.querySelector(".search-bar");
+  form.addEventListener("submit", handleSubmit);
+}
+
+// Change of F to C
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+// Time
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
