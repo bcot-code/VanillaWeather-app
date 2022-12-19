137dd5ee2a061c50ed74628a65f44d6d648230d8
Barbie <78989054+bcot-code@users.noreply.github.com>
2022-12-18 20:45:49 -0600

    updated structure of html & js

--- src/script.js
+++ src/script.js
@@ -1,18 +1,43 @@
 // Location
+function weather(response) {
+  console.log();
+  let temperature = Math.round(response.data.main.temp);
+
+  let location = document.querySelector("#city");
+  location.innerHTML = `${temperature}.`;
+}
+function showPosition(position) {
+  let apiKey = "b9ba0314a93083136d968577c718e31d";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}`;
+  axios.get(apiUrl).then(weather);
+}
+navigator.geolocation.getCurrentPosition(showPosition);
 
 function showTemperature(response) {
   const { name } = response.data;
   const { icon, description } = response.data.weather[0];
   const { temp } = response.data.main;
-  document.querySelector(".city").innerHTML = name;
-  document.querySelector(".icon").src =
+  const { speed } = data.wind;
+  console.log(name, icon, description, temp);
+  document.querySelector("#city").innerHTML = name;
+  document.querySelector("#icon").src =
     "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-  document.querySelector(".description").innerHTML = description;
+  document.querySelector("#description").innerHTML = description;
   document.querySelector("#temperature").innerHTML = temp;
-  windElment.innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
   document.body.style.backgroundImage =
-    "url('https://source.unsplash.com/1600x900/?" + name + ")";
+    "url('https://source.unsplash.com/1600x900/?" + name + "')";
 }
+
+document
+  .querySelector("#search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
 
 function search(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
@@ -22,42 +47,37 @@ function search(city) {
 
 function handleSubmit(event) {
   event.preventDefault();
-  let cityInputElement = document.querySelector(".search-bar");
+  let cityInputElement = document.querySelector("#search-input");
   search(cityInputElement.value);
-  let form = document.querySelector(".search-bar");
-  form.addEventListener("submit", handleSubmit);
 }
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
 // Change of F to C
-function displayFahrenheitTemperature(event) {
+function convertTf(event) {
   event.preventDefault();
+  celLink.classList.remove("active");
+  fehLink.classList.add("active");
+  let temperaElment = document.querySelector("#temperature");
+  let tempF = (displayWeather * 9) / 5 + 32;
+  temperaElment.innerHTML = Math.round(tempF);
+}
+function convertTc(event) {
+  event.preventDefault();
+  fehLink.classList.remove("active");
+  celLink.classList.add("active");
   let temperatureElement = document.querySelector("#temperature");
-
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+  temperatureElement.innerHTML = Math.round(displayWeather);
 }
 
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
+let fahrenheitLink = document.querySelector("#Feh");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
-let celsiusLink = document.querySelector("#celsius-link");
+let celsiusLink = document.querySelector("#Cel");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 // Time
-
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
+function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -67,6 +87,7 @@ function formatDate(timestamp) {
     minutes = `0${minutes}`;
   }
 
+  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -76,6 +97,10 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
-  let day = days[date.getDay()];
+  let day = days[dayIndex];
+
   return `${day} ${hours}:${minutes}`;
 }
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
