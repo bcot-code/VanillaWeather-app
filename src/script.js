c2441217fec33607d761532c7682434d810416fd
Barbie <78989054+bcot-code@users.noreply.github.com>
2022-12-20 01:58:36 -0600

    changed

--- src/script.js
+++ src/script.js
@@ -11,31 +11,59 @@ function getCurrentLocation(event) {
 let currentLocationButton = document.querySelector("#location-dot1");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
+//forecast 10 days
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHtml = `<div class="row">`;
+  let days = ["Thur", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="col-2">
+        <div class="forecast-time">
+          <div class="weather-forecast-date">Sat</div>
+          <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="36" />
+          </div>
+          <canvas width="20" height="20">
+          </canvas>
+          <div class="forecast-temperature">
+            <span class="forecast-temperature-max">
+              13°
+            </span>
+            |
+            <span class="forecast-temperature-min">
+              10°
+            </span>
+          </div>
+        </div>
+      </div>
+
+  `;
+  });
+
+  forecastHtml = forecastHtml + `</div>`;
+  forecastElement.innerHTML = forecastHtml;
+}
+
+//Api
 function displayWeatherCondition(response) {
   let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
   let cityElement = document.querySelector("#city");
-  let windElement = document.querySelector(".wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
+  let windElement = document.querySelector("#wind");
+  let icon = response.data.weather[0].icon;
+
   celsiusTemperature = response.data.main.temp;
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   descriptionElement.innerHTML = response.data.weather[0].description;
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
-  iconElement.setAttribute(
-    "src",
-    "http://openweathermap.org/img/wn/" +
-      response.data.weather[0].icon +
-      "@2x.png"
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  //  I can't change the background img once clicked on search for a particular city or get the date to display ...
-  document.body.style.backgroundImage =
-    "url('https://source.unsplash.com/1600x900/?" + cityElement + "')";
+  document.body.style.backgroundImage = `url(\`https://source.unsplash.com/1600x900/?${cityElement}\`)`;
+  document.querySelector("#icon").src =
+    "https://openweathermap.org/img/wn/" + icon + ".png";
   //
   search(response.data.coord);
 }
@@ -66,12 +94,16 @@ form.addEventListener("submit", handleSubmit);
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let fahrenheiTemp = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheiTemp);
 }
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 let fahrenheitLink = document.querySelector("#Feh");
@@ -83,7 +115,8 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 let celsiusTemperature = null;
 
 // Time
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -93,7 +126,6 @@ function formatDate(date) {
     minutes = `0${minutes}`;
   }
 
-  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -103,10 +135,10 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  let day = days[dayIndex];
-
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-let dateElement = document.getElementById("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
+
+search("San Francisco");
+
+displayForecast();
